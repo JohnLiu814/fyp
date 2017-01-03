72869b3e921ca61e32df178b3c15dc63939c8e0d
JohnLiu814 <hyliuaf@connect.ust.hk>
2017-01-03 19:50:55 +0800

    update navi bar and login

--- js/modernizr.js
+++ js/modernizr.js
@@ -6,7 +6,7 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("firebaseui-auth-container").style.display="block";
 }
 
 // Close the dropdown menu if the user clicks outside of it
